40cd34f7adc6b535cfcae02e61c50fd95cc6cc3d
Александр <alexanderkuchta@gmail.com>
2019-10-28 14:49:35 +0300

    release ver

--- search.go
+++ search.go
@@ -7,48 +7,28 @@ import (
 
 func Search (desiredNum int, Arr []int) (int, error, int) {
 	// сначала проверяем, отсортирован ли массив
-	if !checkSort(Arr) {
+	if sort.IntsAreSorted(Arr) {
 		sort.Ints(Arr)
 	}
 
-	return deepSearch(desiredNum, Arr)
-}
-
-func checkSort (Arr []int) bool {
-	// Если массив из одного элемента, то он отсортирован
-	if len(Arr) == 1 {
-		return true
-	}
-	// если элементов больше одного, то проверка
-	for i := 1; i < len(Arr); i++ {
-		if Arr[i-1] > Arr[i] {
-
-			return false
-		}
-	}
-
-	return true
-}
-
-func deepSearch (desiredNum int, Arr []int) (int, error, int) {
 	// определяем границы поиска
 	low := 0
 	high := len(Arr) - 1
 
-	for countIteration := 1; low <= high ; countIteration++{
-
+	for countIteration := 1; low <= high ; countIteration++ {
 		mid := (low + high) / 2 // ищем средний элемент
 		guess := Arr[mid]		 // по элементу достаем число
+
 		if guess == desiredNum { // нашли число, ура!
 
 			return mid, nil, countIteration
 		}
 
 		if guess > desiredNum {  // если центральное число больше искомого, сдвигаем верхнюю границу
-			high = mid -1
+			high = mid - 1
 		}
 
-		if guess < desiredNum {				 // если центрально число меньше искомого, сдвигаем нижнюю границу
+		if guess < desiredNum {	 // если центрально число меньше искомого, сдвигаем нижнюю границу
 			low = mid + 1
 		}
 	}
